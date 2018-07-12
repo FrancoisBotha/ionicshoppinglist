92b9a3f91e79bd06f97dc323a3496a1faf47571e
FrancoisBotha <francois@ims.com.na>
2018-07-12 15:38:34 +0200

    Add icon to header on the right

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
 import { ShoppingListService } from '../../services/shopping-list';
+import { Ingredient } from '../../models/ingredient';
 
 @IonicPage()
 @Component({
@@ -9,14 +10,30 @@ import { ShoppingListService } from '../../services/shopping-list';
   templateUrl: 'shopping-list.html',
 })
 export class ShoppingListPage {
+  listItems: Ingredient[];
 
   constructor (private slService: ShoppingListService) {}
+
+  //Does not cache - always loads when page enters
+  ionViewWillEnter() {
+    this.loadItems();
+  }
 
   onAddItem(form: NgForm) {
     console.log(form);
     this.slService.addItem(form.value.ingredientName,
                             form.value.amount);
     form.reset(); //empty form and reset validation state
+    this.loadItems();
+  }
+
+  private loadItems() {
+    this.listItems = this.slService.getItems();
+  }
+
+  onCheckItem(index: number) {
+    this.slService.removeItem(index);
+    this.loadItems();
   }
 
 }
